e8521b21e8e3594fa77f298bd432d83ea1c4dba8
John Chen <johnchen062094@gmail.com>
2020-04-25 23:03:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
-  if integer % 3 == 0
-    "Fizz"
-  elsif integer % 5 == 0 
-    "Buzz"
-  elsif integer % 5 == 0 && integer % 3 == 0 
+  if integer % 3 == 0 && integer % 5 == 0 
     "FizzBuzz"
-  end
 end
